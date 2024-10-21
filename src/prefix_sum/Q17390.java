5ec43dfea6388f8356b3a946a0f7927e295b330e
Hyun <ghkdgus29@naver.com>
2024-10-21 09:26:17 +0900

    Create Q17390.java

---
+++ src/prefix_sum/Q17390.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Q17390 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int len = Integer.parseInt(st.nextToken());
+        int cnt = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[len];
+        long[] s = new long[len+1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<len; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        
+        for(int i=1; i<=len; i++) {
+            s[i] = s[i-1] + a[i-1];
+        }
+
+        for(int i=0; i<cnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(s[end] - s[start-1]);
+        }
+        
+    }
+}
