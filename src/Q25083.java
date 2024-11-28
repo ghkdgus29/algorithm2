be715cba7fb28ce29d8112ef932a0e7f4bc541e7
Hyun <ghkdgus29@naver.com>
2024-11-28 21:32:19 +0900

    24-11-28

---
+++ src/Q25083.java
@@ -0,0 +1,13 @@
+public class Q25083 {
+	public static void main(String[] args) {
+		String text = """
+			         ,r'"7
+			r`-_   ,'  ,/
+			 \\. ". L_r'
+			   `~\\/
+			      |
+			      |
+			""";
+		System.out.println(text);
+	}
+}
